22473023fb7b5ca1f8f1469fc3f8ea10dd9e0ff7
Xeno RHODA <xeno@zarobi.co.za>
2019-12-02 13:57:39 +0200

    Changed callback

--- public/Hitable.js
+++ public/Hitable.js
@@ -1,4 +1,5 @@
 import Sphere from './Sphere.js'
+import Vector from './Vector.js';
 
 class Hitable_List {
 	constructor(list, size){
@@ -8,23 +9,28 @@ class Hitable_List {
 
 	hit(ray, tmin, tmax, callback){
 		let closest = tmax;
-		for (let i = 0; i < this.size; i++){
+		let rec = {t: 0.0, p: new Vector(0.0, 0.0, 0.0), n: new Vector(0.0, 0.0, 0.0), hit: false};
+		for (var i = 0; i < this.size; i++){
 			if (this.list[i] instanceof Sphere){
-				this.list[i].is_hit(ray, tmin, closest, (record) => {
+				let tmp_hit = this.list[i].is_hit(ray, tmin, closest, (record) => {
 					if (record.hit){
+						
 						callback(true, record)
 					}
 					else{
 						callback(false, record)
 					}
 				});
+				if (tmp_hit.hit){
+					closest = tmp_hit.t;
+					rec = tmp_hit;
+				}
+					
+					//console.log({rec, i})
+				// closest = rec.t;
 			}
-			// if (temp_record.hit){
-			// 	hit_anything = true;
-			// 	closest = temp_record.hit_record.t;
-			// 	hit_record = temp_record.hit_record;
-			// }
 		}
+		return (rec)
 		// return {hit_anything: hit_anything, hit_record: hit_record}
 	}
 }
