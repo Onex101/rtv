f27fb7bd0ff565bd3942e9f1ae06c55ce6313b1b
Xeno RHODA <xeno@zarobi.co.za>
2019-12-02 13:57:53 +0200

    Changed Callback to return

--- public/Sphere.js
+++ public/Sphere.js
@@ -9,32 +9,27 @@ class Sphere{
 	is_hit(ray, tmin, tmax, callback){
 		let oc = ray.origin().sub(this.center);
 		let a = ray.direction().dot(ray.direction());
-		let b = 2.0 * oc.dot(ray.direction());
+		let b = oc.dot(ray.direction());
 		let c = oc.dot(oc) - Math.pow(this.radius, 2);
-		let discriminant = b*b - 4*a*c;
-		let record = {};
-		if (discriminant < 0){
-			let temp = (-b - Math.sqrt(b*b-a*c))/a;
+		let discriminant = b*b - a*c;
+		let record = {t: 0.0, p: new Vector(0.0, 0.0, 0.0), n: new Vector(0.0, 0.0, 0.0), hit: false};
+		if (discriminant > 0){
+			let temp = (-b - Math.sqrt(discriminant))/a;
 			if (temp < tmax && temp > tmin){
 				record.t = temp;
 				record.p = ray.point_at_parameter(temp);
 				record.n = record.p.sub(this.center).div(this.radius);
 				record.hit = true;
-				callback(record);
 			}
-			temp = (-b + Math.sqrt(b*b-a*c))/a;
+			temp = (-b + Math.sqrt(discriminant))/a;
 			if (temp < tmax && temp > tmin){
 				record.t = temp;
 				record.p = ray.point_at_parameter(temp);
 				record.n = record.p.sub(this.center).div(this.radius);
 				record.hit = true;
-				callback(record);
 			}
 		}
-		else{
-			record.hit = false;
-			callback(record);
-		}
+		return record;
 			
 	}
 
