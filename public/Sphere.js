c5a97132a4b8a315b440ac167c9c62f6e1c5ad6d
Xeno RHODA <xeno@zarobi.co.za>
2019-12-03 14:00:42 +0200

    Sphere class updated with Material

--- public/Sphere.js
+++ public/Sphere.js
@@ -1,9 +1,10 @@
 import Vector from "./Vector.js";
 
 class Sphere{
-	constructor(center, radius){
+	constructor(center, radius, material){
 		this.center = center || new Vector ();
 		this.radius = radius || 1;
+		this.material = material || new Material ();
 	}
 
 	is_hit(ray, tmin, tmax){
@@ -12,13 +13,14 @@ class Sphere{
 		let b = oc.dot(ray.direction());
 		let c = oc.dot(oc) - this.radius * this.radius;
 		let discriminant = b*b - a*c;
-		let record = {t: 0.0, p: new Vector(0.0, 0.0, 0.0), n: new Vector(0.0, 0.0, 0.0), hit: false};
+		let record = {t: 0.0, p: new Vector(0.0, 0.0, 0.0), n: new Vector(0.0, 0.0, 0.0), hit: false, m: this.material};
 		if (discriminant > 0){
 			let temp = (-b - Math.sqrt(discriminant))/a;
 			if (temp < tmax && temp > tmin){
 				record.t = temp;
 				record.p = ray.point_at_parameter(temp);
 				record.n = record.p.sub(this.center).div(this.radius);
+				record.m = this.material;
 				record.hit = true;
 			}
 			temp = (-b + Math.sqrt(discriminant))/a;
@@ -26,6 +28,7 @@ class Sphere{
 				record.t = temp;
 				record.p = ray.point_at_parameter(temp);
 				record.n = record.p.sub(this.center).div(this.radius);
+				record.m = this.material;
 				record.hit = true;
 			}
 		}
